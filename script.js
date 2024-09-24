11e28b10c899bbfa45b7fea38b174b1d8fb88243
apelarex <163296991+apelarex@users.noreply.github.com>
2024-09-24 08:17:55 -0300

    cogido terminado

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ for (var i=0; i < pacientes.length; i++) {
 
 var paciente = pacientes[i];
 
-var tdPeso paciente.querySelector(".info-peso");
+var tdPeso = paciente.querySelector(".info-peso");
 
-var peso tdPeso.textContent;
+var peso = tdPeso.textContent;
 
-var tdAltura paciente.querySelector(".info-altura");
+var tdAltura = paciente.querySelector(".info-altura");
 
-var altura tdAltura.textContent;
+var altura = tdAltura.textContent;
 
-var tdImc paciente.querySelector(".info-imc");
+var tdImc = paciente.querySelector(".info-imc");
 }
 
 
